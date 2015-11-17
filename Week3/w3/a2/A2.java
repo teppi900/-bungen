f75f5ff51b905a9a460149867a969a3be84bdcde
teppi900 <tepp99x@gmail.com>
2015-11-17 22:05:52 +0100

    Update A2.java

--- Week3/w3/a2/A2.java
+++ Week3/w3/a2/A2.java
@@ -7,25 +7,25 @@ import java.io.FileReader;
 public class A2 {
 	public static void main(String[] args) {
 		try {	
-			BufferedReader in=new BufferedReader(new FileReader("D:Kontostaende.txt"));		//BufferedReader
-			while(in.ready()){																//while buffer not empty==true
+			BufferedReader in=new BufferedReader(new FileReader("D:Kontostaende.txt"));	//BufferedReader
+			while(in.ready()){								//while buffer not empty==true
 				try{
-				String a=in.readLine();														//readLine
+				String a=in.readLine();							//readLine
 				Character ch=new Character(',');	
 				Character ch2=new Character('.');
-				a=a.replace( ch , ch2);														//convert "," to "."
-				double zahl=Double.parseDouble(a);											//convert String to double
-				if(zahl>100.0){																//print numbers over 100.0
+				a=a.replace( ch , ch2);							//convert "," to "."
+				double zahl=Double.parseDouble(a);					//convert String to double
+				if(zahl>100.0){								//print numbers over 100.0
 					System.out.println(zahl);
-				}																		//end of if
+				}								//end of if
 				}catch(NumberFormatException e){
 					e.printStackTrace();
 				}
-			}																			//end of while
-			in.close();																		//close BufferedReader
-		} catch (Exception e) {
+			}									//end of while
+			in.close();									//close BufferedReader
+		} catch (Exception e) {																																
 			System.out.println(e);// TODO: handle exception
 		}
 
-	}																					//end of main()
-}																						//end of A2
+	}											//end of main()
+}												//end of A2
