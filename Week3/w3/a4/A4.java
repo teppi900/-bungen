4b78f64ee436820dcdb6847100532523ac104b64
teppi900 <tepp99x@gmail.com>
2015-11-17 22:08:25 +0100

    Update A4.java

--- Week3/w3/a4/A4.java
+++ Week3/w3/a4/A4.java
@@ -6,26 +6,26 @@ public class A4 {
 		Scanner sc=new Scanner(System.in);
 		double max;
 		double min;
-		System.out.println("Wie viele Kontost‰nde?");			// length of array
+		System.out.println("Wie viele Kontost√§nde?");		// length of array
 		int konto=sc.nextInt();
 		double[]account=new double[konto];						
 		double[]oldAccount;
-		for (int j = 0; j <account.length; j++) {				// assign variable to the array
+		for (int j = 0; j <account.length; j++) {		// assign variable to the array
 			System.out.println("Kontostand "+(j+1)+": ");		
 			double z=sc.nextDouble();							
 			account[j]=z;										
 		}														
-		oldAccount=account.clone();								// cloning account
+		oldAccount=account.clone();				// cloning account
 		sc.close();
 		double x;
-		for (int j = 0; j < account.length-1; j++) {			//bubblesort
+		for (int j = 0; j < account.length-1; j++) {		//bubblesort
 			if (account[j]>account[j+1]) {
 				x=account[j];
 				account[j]=account[j+1];
 				account[j+1]=x;
 			}
 		}	
-		min=account[0];											//calculation and stuff
+		min=account[0];						//calculation and stuff
 		max=account[account.length-1];
 		double sum=0;												
 		for (int j = 0; j < account.length; j++) {
@@ -33,7 +33,7 @@ public class A4 {
 		}
 		double mittel = sum/account.length;
 		
-		for (int j = 0; j < account.length; j++) {				//print account balance, min, max and the average value
+		for (int j = 0; j < account.length; j++) {		//print account balance, min, max and the average value
 			System.out.println("Kontostand: "+oldAccount[j]);	
 		}
 		System.out.println("min: "+min);
