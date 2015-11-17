a83f48bd14b5b9fbb0a34a5bd060f16b0afae86d
teppi900 <tepp99x@gmail.com>
2015-11-17 22:09:55 +0100

    Update A5.java

--- Week3/w3/a5/A5.java
+++ Week3/w3/a5/A5.java
@@ -10,46 +10,46 @@ public class A5 {
 	private static double min;
 	private static Scanner sc=new Scanner(System.in);
 	
-		public static void start(){										//start method
-			System.out.println("Wie viele Kontost‰nde?");				//amount of accounts
+		public static void start(){						//start method
+			System.out.println("Wie viele Kontost√§nde?");			//amount of accounts
 			int konto=sc.nextInt();
 			account=new double[konto];
-			for (int j = 0; j <account.length; j++) {					//assign variable to array
+			for (int j = 0; j <account.length; j++) {			//assign variable to array
 				System.out.println("Kontostand "+(j+1)+": ");
 				double z=sc.nextDouble();
 				account[j]=z;												
-				oldAccount=account.clone();								//clone account
-				}														//end of for
-		}																//end of start()
+				oldAccount=account.clone();				//clone account
+				}						//end of for
+		}								//end of start()
 		
-		public static void rechner(){									//rechner method									
-			min=account[0];												//min	
-			max=account[account.length-1];								//max
+		public static void rechner(){						//rechner method									
+			min=account[0];							//min	
+			max=account[account.length-1];					//max
 			double sum=0;											
 			for (int j = 0; j < account.length; j++) {
 				sum=sum+account[j];
 			}
-			double mittel = sum/account.length;							//sum
+			double mittel = sum/account.length;				//sum
 			
-			for (int j = 0; j < account.length; j++) {					//print
+			for (int j = 0; j < account.length; j++) {			//print
 				System.out.println("Kontostand: "+oldAccount[j]);
 			}
 			System.out.println("min: "+min);
 			System.out.println("max: "+max);
 			System.out.println("mittel "+mittel);
-		}																//end of rechner()
+		}								//end of rechner()
 		
-		public static void bubble(){									//bubble method
+		public static void bubble(){						//bubble method
 				double x;
 				for (int j = 0; j < account.length-1; j++) {			
 					if (account[j]>account[j+1]) {
 						x=account[j];
 						account[j]=account[j+1];
 						account[j+1]=x;
-					}													//end of if
-				}														//end of for
-		}																//end of bubble()
-		public static void main(String[] args) {						//main()
+					}					//end of if
+				}						//end of for
+		}								//end of bubble()
+		public static void main(String[] args) {				//main()
 			try {
 				start();
 				bubble();
