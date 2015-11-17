1b7cf21eb3c6e08efa333fc7cf7ae51e145dcb15
teppi900 <tepp99x@gmail.com>
2015-11-17 22:07:15 +0100

    Update A3.java

--- Week3/w3/a3/A3.java
+++ Week3/w3/a3/A3.java
@@ -8,26 +8,26 @@ public class A3 {
 		try{
 		BufferedReader in =new BufferedReader(new FileReader("D:Kontostaende.txt"));	//BufferedReader
 		
-		while(in.ready()){																//while buffer not empty==true
+		while(in.ready()){								//while buffer not empty==true
 			try {
-			String line=in.readLine();													//readLine
+			String line=in.readLine();						//readLine
 			Character sc= new Character(',');
 			Character sc2=new Character('.');
-			line=line.replace(sc, sc2);													//convert "," to "."
-			double star=Double.parseDouble(line);										//convert String to double
-				for (int i = 0; i < star; i=i+10) {										//draw stars
+			line=line.replace(sc, sc2);						//convert "," to "."
+			double star=Double.parseDouble(line);					//convert String to double
+				for (int i = 0; i < star; i=i+10) {				//draw stars
 					System.out.print("*");
-				}																		//end of for
+				}							//end of for
 				System.out.print("\n");
 				}catch(Exception e){
 					System.out.println(e);
 				}
-		}																				//end of while
-		in.close();																		//close BufferedReader
+		}									//end of while
+		in.close();									//close BufferedReader
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		
 		
-	}																					//end of main()
-}																						//end of A3
+	}										//end of main()
+}											//end of A3
